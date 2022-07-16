654b7c2b414ffdd65c9c99b2bd19ae9d0c720981
Dylan Arlich <d.arlich42@gmail.com>
2022-07-16 13:59:16 -0700

    Make it so drawing grid erases previous grid

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-const container = document.querySelector(".container");
+let sides;
 
 const btn = document.querySelector(".button");
 btn.addEventListener("click", drawGrid);
 
 function drawGrid() {
-  let sides = prompt("How many pixels per side?");
-
-  container.style.gridTemplateColumns = `repeat(${sides}, 1fr)`;
-  container.style.gridTemplateRows = `repeat(${sides}, 1fr)`;
+  let board = document.querySelector(".board");
+  let pixels = board.querySelectorAll("div");
+  pixels.forEach((div) => div.remove());
+  promptSides();
+  board.style.gridTemplateColumns = `repeat(${sides}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${sides}, 1fr)`;
 
   for (i = 0; i < sides * sides; i++) {
     const pixel = document.createElement("div");
     pixel.classList.add("pixel");
+
     pixel.addEventListener("mouseover", () => pixel.classList.add("color"));
 
-    container.appendChild(pixel);
+    board.appendChild(pixel);
   }
 }
 
+function promptSides() {
+  sides = prompt("How many pixels per side? 1 - 64");
+}
 //TO DO:
 
-//Make Drawing the new grid erase the previous one
-// in the same function something that iterates through all the previous pixels and deletes them before making the new ones
 //Make some rules so the the prompt will only work with numbers
 // should have some logic like "is NaN"
 
